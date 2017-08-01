37a0d8c63f218c5b835533d8cf1688d33138e08c
300931446 <Harmanpreet.preetsingh@gmail.com>
2017-08-01 17:12:07 -0400

    Added Project Files and Cretes Demo Project

---
+++ COMP123-S2017-Lesson12B/COMP123-S2017-Lesson12B/DemoForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Name: Harmanpreet Singh
+ * Student Id:300931446
+ * date: Sugust 1,2017
+ * Description: Demo Application to showcase windows forms and UI controls
+ * Version 0.1: Project was Created 
+ */
+namespace COMP123_S2017_Lesson12B
+{
+    public partial class DemoForm : Form
+    {
+        public DemoForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ COMP123-S2017-Lesson12B/COMP123-S2017-Lesson12B/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Name: Harmanpreet Singh
+ * Student Id:300931446
+ * date: Sugust 1,2017
+ * Description: Demo Application to showcase windows forms and UI controls
+ * Version 0.1: Project was Created 
+ */
+
+namespace COMP123_S2017_Lesson12B
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new DemoForm());
+        }
+    }
+}
